17bc238ba8b8bba7deaaf5ab496fbeb2f519743f
dimitarstoyanov90 <dimitarstoyanov90@gmail.com>
2014-06-15 21:38:58 +0300

    New css hover and js functunality added
    wall movement added hover effects added

--- js/wallMovement.js
+++ js/wallMovement.js
@@ -1,3 +1,9 @@
-$("menuitem").mouseenter(function(){
-  
+$(document).ready(function(){
+  $("#leftWall").click(function(){
+    $(this).hide(2000);
+  	
+  });
+   $("#rightWall").click(function(){
+    $(this).hide(2000);
+  });
 });
